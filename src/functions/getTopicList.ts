cede6ba72b96c417a8883224b59e455b4ef9a535
gokhangunduz <me@gokhangunduz.com.tr>
2023-10-06 18:00:54 +0300

    refacor(funcs): :tada: add functions

---
+++ src/functions/getTopicList.ts
@@ -0,0 +1,18 @@
+export default function getTopicList() {
+  const mockData = [
+    {
+      name: "topic1",
+      type: "std_msgs/msg/String",
+    },
+    {
+      name: "topic2",
+      type: "std_msgs/msg/String",
+    },
+    {
+      name: "topic3",
+      type: "std_msgs/msg/String",
+    },
+  ];
+
+  return mockData;
+}
--- src/functions/rosMetricListener.ts
+++ src/functions/rosMetricListener.ts
@@ -1,12 +1,10 @@
 import rosClient from "../clients/rosClient";
-import getTopicList from "../data/getTopicList";
+import getTopicList from "../functions/getTopicList";
 
 export default function rosMetricListener() {
-  getTopicList()
-    .filter((topic: any) => topic.function === "get")
-    .map((topic: any) => {
-      rosClient.createSubscription(topic.type, topic.name, (msg: any) => {
-        console.log(`${msg.data}`);
-      });
+  getTopicList().map((topic: any) => {
+    rosClient.createSubscription(topic.type, topic.name, (msg: any) => {
+      console.log(`${msg.data}`);
     });
+  });
 }
